6bbf08adabf0523978e013eebc8cbf8239c73393
Sinthoras39 <digc0820@gmail.com>
2023-01-14 21:27:17 +0100

    DHT22 inside & outside, BMP180 pressure

---
+++ main.py
@@ -0,0 +1,54 @@
+import csv
+from sys import argv
+import matplotlib.pyplot as plt
+
+def get_name(s):
+    for i in s:
+        if not i.isnumeric():
+            return s[s.find(i):], s[:s.find(i)]
+
+
+def main():
+    if len(argv) < 1:
+        print("Please put the filename as parameter.")
+
+    f1 = []
+    t1 = []
+
+    t2 = []
+    f2 = []
+
+    f3 = []
+    t3 = []
+
+    with open(argv[1], newline="") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        for row in spamreader:
+            query = get_name(row[0])
+            if query[0].startswith("F1") or query[0].startswith("F2") or query[0].startswith("BAR"):
+                data = (int(query[1]) / 60000, float(query[0].split(":")[1]))
+                if query[0].startswith("F1"):
+                    f1.append(data[1])
+                    t1.append(data[0])
+                elif query[0].startswith("F2"):
+                    f2.append(data[1])
+                    t2.append(data[0])
+                elif query[0].startswith("BAR"):
+                    f3.append(data[1])
+                    t3.append(data[0])
+
+    print("Arrays created. Displaying Plot...")
+
+    plt.title("Außen(Blau DHT22)- und Innentemperatur(Rot DHT22, Grün BMP180)")
+    plt.xlabel("Zeit in Minuten")
+    plt.ylabel("Temperatur in °C")
+    plt.plot(t1, f1,
+             t2, f2,
+             t3, f3)
+
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
